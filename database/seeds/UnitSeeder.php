0a365969721e50f467b2b422327299bb0c911461
alucarth <ltorrezs2008@gmail.com>
2019-07-01 11:01:29 -0400

    importacion de empleados terminado

--- database/seeds/UnitSeeder.php
+++ database/seeds/UnitSeeder.php
@@ -13,10 +13,33 @@ class UnitSeeder extends Seeder
     {
         //
         $unities = [
-            ['name' => 'PROYECTOS','managament_id'=>2],
+            ['name' => 'PROYECTOS','managament_id'=>2],//1
             ['name' => 'NORMAS Y CALIDAD','managament_id'=>2],
             ['name' => 'PLANIFICACION','managament_id'=>2],
             ['name' => 'SISTEMAS','managament_id'=>2],
+
+            ['name' => 'SERVICIOS GENERALES','managament_id'=>3],//5
+            ['name' => 'ACTIVOS FIJOS','managament_id'=>3],
+            ['name' => 'TRIBUTARIA','managament_id'=>3],
+            ['name' => 'PRESUPUESTOS','managament_id'=>3],
+            ['name' => 'TESORERIA','managament_id'=>3],
+            ['name' => 'CONTABILIDAD','managament_id'=>3],//10
+            ['name' => 'RECURSOS HUMANOS','managament_id'=>3],
+            ['name' => 'PRESUPUESTOS','managament_id'=>3],
+            ['name' => 'CONTRATACIONES','managament_id'=>3],
+            ['name' => 'FINANCIERA','managament_id'=>3],
+            ['name' => 'ARCHIVO VILLA FATIMA','managament_id'=>3],//15
+
+            ['name' => 'PRODUCCION','managament_id'=>4],
+            ['name' => 'APROVISIONAMIENTO','managament_id'=>4],
+            ['name' => 'MANTENIMIENTO','managament_id'=>4],
+            ['name' => 'CALIDAD','managament_id'=>4],
+
+            ['name' => 'DESAYUNO ESCOLAR VILLA FATIMA','managament_id'=>5],
         ];
+
+        foreach ($unities as $unit) {
+            App\Unity::create($unit);
+        }
     }
 }
