daa38d17746ce900bee4de618ebeb28611e9a236
alucarth <ltorrezs2008@gmail.com>
2019-11-13 17:10:29 -0400

    trabajando en kardex de marcados

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -21,6 +21,108 @@ class AttendanceController extends Controller
         //
         $employee = Auth::user()->employee;
         $date = Carbon::now();
+        $day=explode('-',$date)[2];
+        $days=cal_days_in_month(CAL_GREGORIAN,$date->month , $date->year);
+        $month = $date->month;
+        $year = $date->year;
+        // $employee = Employee::find(23);
+        $attendances = [];
+        for ($d=1; $d <= $days ; $d++)
+        {
+            //buscar marcados solo para los horarios del dia XD
+            if($d<=$day)
+            {
+                foreach($employee->type_hours as $type_hour)
+                {
+                    //falta mas informacion en el type_hour
+
+                    $attendance_entry = AttendanceEmployee::where('date',$year.'-'.$month.'-'.$d)
+                                            // ->whereBetween('time',[$type_hour->start_of_entry, $type_hour->end_of_entry])
+                                            ->where('time','>=',$type_hour->start_of_entry)
+                                            ->where('time','<=',$type_hour->end_of_entry)
+                                            ->where('employee_id',$employee->id)
+                                            ->first();
+                    if($attendance_entry)
+                    {
+
+                        $array_start_time = explode(':',$type_hour->start_of_entry);
+                        $array_tolerance_time = explode(':',$type_hour->tolerance_entry);
+                        $minutes = (int) $array_start_time[1] + (int) $array_tolerance_time[1];
+                        $hours = (int) $array_start_time[0] + (int) $array_tolerance_time[0];
+                        if($minutes >=60)
+                        {
+                            $minutes -= 60;
+                            $hours++;
+                        }
+                        $time = $hours.':'.$minutes;
+                        if($attendance_entry->time >=$type_hour->start_of_entry && $attendance_entry->time <= $time)
+                        {
+                            $attendance_entry->state = 'success';
+                        }else
+                        {
+                            $attendance_entry->state = 'warning';
+                        }
+                        array_push($attendances,$attendance_entry);
+                    }else
+                    {
+                        if(strlen($d)<2)
+                        {
+                            $attendance_entry = array('date'=>$year.'-'.$month.'-0'.$d,'time'=> 'Sin Entrada','state'=>'error');
+                        }else{
+                            $attendance_entry = array('date'=>$year.'-'.$month.'-'.$d,'time'=> 'Sin Entrada','state'=>'error');
+                        }
+                        array_push($attendances,$attendance_entry);
+                    }
+
+                    $attendance_output = AttendanceEmployee::where('date',$year.'-'.$month.'-'.$d)
+                                            // ->whereBetween('time',[$type_hour->start_of_output, $type_hour->end_of_output])
+                                            ->where('time','>=',$type_hour->start_of_output)
+                                            ->where('time','<=',$type_hour->end_of_output)
+                                            ->where('employee_id',$employee->id)
+                                            ->first();
+                    if($attendance_output)
+                    {
+
+                        // $array_start_time = explode(':',$type_hour->start_of_output);
+                        // $array_tolerance_time = explode(':',$type_hour->tolerance_output);
+                        // $minutes = (int) $array_start_time[1] + (int) $array_tolerance_time[1];
+                        // $hours = (int) $array_start_time[0] + (int) $array_tolerance_time[0];
+                        // if($minutes >=60)
+                        // {
+                        //     $minutes -= 60;
+                        //     $hours++;
+                        // }
+                        // $time = $hours.':'.$minutes;
+                        if($attendance_output->time >=$type_hour->output && $attendance_output->time <= $type_hour->end_of_output)
+                        {
+                            $attendance_output->state = 'success';
+                        }else
+                        {
+                            $attendance_output->state = 'warning';
+                        }
+                        array_push($attendances,$attendance_output);
+                    }else
+                    {
+                        if(strlen($d)<2)
+                        {
+                            $attendance_entry = array('date'=>$year.'-'.$month.'-0'.$d,'time'=> 'Sin Salida','state'=>'error');
+                        }else{
+                            $attendance_entry = array('date'=>$year.'-'.$month.'-'.$d,'time'=> 'Sin Salida','state'=>'error');
+                        }
+                        array_push($attendances,$attendance_entry);
+                    }
+                }
+            }
+
+        }
+        return response()->json(compact('attendances'));
+    }
+
+    public function attendance()
+    {
+
+        $employee = Employee::find(23);
+        $date = Carbon::now();
         $days=cal_days_in_month(CAL_GREGORIAN,$date->month , $date->year);
         $month = $date->month;
         $year = $date->year;
@@ -32,8 +134,7 @@ class AttendanceController extends Controller
             foreach($employee->type_hours as $type_hour)
             {
                 //falta mas informacion en el type_hour
-                // Log::info('inicio '.$type_hour->start_of_entry);
-                // Log::info('fin '.$type_hour->end_of_entry);
+
                 $attendance_entry = AttendanceEmployee::where('date',$year.'-'.$month.'-'.$d)
                                         // ->whereBetween('time',[$type_hour->start_of_entry, $type_hour->end_of_entry])
                                         ->where('time','>=',$type_hour->start_of_entry)
@@ -42,9 +143,7 @@ class AttendanceController extends Controller
                                         ->first();
                 if($attendance_entry)
                 {
-                    Log::info('inicio '.$type_hour->start_of_entry);
-                    Log::info('fin '.$type_hour->end_of_entry);
-                    Log::info(json_encode($attendance_entry));
+
                     $array_start_time = explode(':',$type_hour->start_of_entry);
                     $array_tolerance_time = explode(':',$type_hour->tolerance_entry);
                     $minutes = (int) $array_start_time[1] + (int) $array_tolerance_time[1];
@@ -63,8 +162,13 @@ class AttendanceController extends Controller
                         $attendance_entry->state = 'warning';
                     }
                     array_push($attendances,$attendance_entry);
+                }else
+                {
+                    $attendance_entry = array('date'=>$year.'-'.$month.'-'.$d,'time'=> 'Sin Entrada','state'=>'error');
+
+                    array_push($attendances,$attendance_entry);
                 }
-                // Log::info($year.'-'.$month.'-'.$d );
+
                 $attendance_output = AttendanceEmployee::where('date',$year.'-'.$month.'-'.$d)
                                         // ->whereBetween('time',[$type_hour->start_of_output, $type_hour->end_of_output])
                                         ->where('time','>=',$type_hour->start_of_output)
@@ -73,8 +177,7 @@ class AttendanceController extends Controller
                                         ->first();
                 if($attendance_output)
                 {
-                    Log::info('Salida '.$type_hour->start_of_output.' - '.$type_hour->end_of_output );
-                    Log::info(json_encode($attendance_output));
+
                     $array_start_time = explode(':',$type_hour->start_of_output);
                     $array_tolerance_time = explode(':',$type_hour->tolerance_output);
                     $minutes = (int) $array_start_time[1] + (int) $array_tolerance_time[1];
@@ -93,53 +196,9 @@ class AttendanceController extends Controller
                         $attendance_output->state = 'warning';
                     }
                     array_push($attendances,$attendance_output);
-                }
-            }
-
-        }
-        return response()->json(compact('attendances'));
-    }
-
-    public function attendance()
-    {
-        $date = Carbon::now();
-        $days=cal_days_in_month(CAL_GREGORIAN,$date->month , $date->year);
-        $month = $date->month;
-        $year = $date->year;
-        $employee = Employee::find(23);
-        $attendances = [];
-        for ($d=1; $d <= $days ; $d++)
-        {
-            //buscar marcados solo para los horarios del dia XD
-            foreach($employee->type_hours as $type_hour)
-            {
-                //falta mas informacion en el type_hour
-               
-                $attendance_entry = AttendanceEmployee::where('date',$year.'-'.$month.'-'.$d)
-                                        // ->whereBetween('time',[$type_hour->start_of_entry, $type_hour->end_of_entry])
-                                        ->where('time','>=',$type_hour->start_of_entry)
-                                        ->where('time','<=',$type_hour->end_of_entry)
-                                        ->where('employee_id',$employee->id)
-                                        ->first();
-                if($attendance_entry)
-                {
-                    Log::info('inicio '.$type_hour->start_of_entry);
-                    Log::info('fin '.$type_hour->end_of_entry);
+                }else{
+                    $attendance_entry = array('date'=>$year.'-'.$month.'-'.$d,'time'=> 'Sin Salida','state'=>'error');
                     array_push($attendances,$attendance_entry);
-                }
-                
-
-                $attendance_output = AttendanceEmployee::where('date',$year.'-'.$month.'-'.$d)
-                                        // ->whereBetween('time',[$type_hour->start_of_output, $type_hour->end_of_output])
-                                        ->where('time','>=',$type_hour->start_of_output)
-                                        ->where('time   ','<=',$type_hour->end_of_output)
-                                        ->where('employee_id',$employee->id)
-                                        ->first();
-                if($attendance_output)
-                {
-                    Log::info('inicio salida '.$type_hour->start_of_output);
-                    Log::info('fin salida '.$type_hour->end_of_output);
-                    array_push($attendances,$attendance_output);
                 }
             }
 
--- resources/js/views/attendance/Index.vue
+++ resources/js/views/attendance/Index.vue
@@ -6,7 +6,7 @@
         <!-- <v-btn @click="create();" color="primary" dark class="mb-2">Nuevo</v-btn> -->
         </v-card-title>
         <v-card-text>
-            <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins"  locale='es' :events="attendances" />
+            <FullCalendar defaultView="dayGridMonth" :header="{left: 'dayGridMonth,dayGridWeek'}" :plugins="calendarPlugins"  locale='es' :events="attendances" />
         </v-card-text>
         <!-- <edit-biometric :dialog="dialog" :biometric="biometric" @close="close"  @biometric="update"></edit-biometric>
         <sync-biometric :dialog="dialog_sync" :biometric="biometric" @close="close_sync" ></sync-biometric> -->
@@ -20,6 +20,7 @@ export default {
     data:()=>({
         calendarPlugins: [ dayGridPlugin ],
         attendances:[],
+
 
     }),
     mounted()
@@ -37,17 +38,35 @@ export default {
 
                         let days=[];
                         response.data.attendances.forEach(attendance => {
-                            let event = {title:attendance.time, date: `${attendance.date}`,backgroundColor: attendance.state=='success'?'#4CAF50':'#FFEA00', textColor: attendance.state=='success'?'#FFFFFF':'#000000'  }
+                            let event = {title:attendance.time, date: `${attendance.date}`,backgroundColor: this.getColor(attendance), textColor: attendance.state=='success'?'#FFFFFF':'#000000'  }
                             this.attendances.push(event);
                             // days.push(attendance.date.toString().split('-')[2]);
                         });
                         // let day = moment().day();
-                        // for (let i = 0; i < array.day; i++) 
+                        // for (let i = 0; i < array.day; i++)
                         // {
-                            
+
                         // }
                  });
         },
+
+        getColor(attendance)
+        {
+            let color = '#FFFFFF'
+            switch (attendance.state) {
+                case 'success':
+                    color='#4CAF50';
+                    break;
+                case 'warning':
+                    color='#FFEA00';
+                    break;
+                case 'error':
+                    color='#FF8A80';
+                    break;
+
+            }
+            return color;
+        }
 
     },
     components: {
