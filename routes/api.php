868ad79affd2d86b456f89333adea3121f72f94a
alucarth <ltorrezs2008@gmail.com>
2019-11-27 18:48:35 -0400

    planilla primera version

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,8 @@ Route::get('ficha_personal/{id}', 'ReportController@ficha_personal');
 Route::get('employee_request_print/{id}', 'ReportController@boleta');
 Route::get('attendance_employee/{employee_id}','ReportController@attendance_employee');
 Route::get('attendance_employee_date/{employee_id}/{from_date}/{to_date}','ReportController@attendance_employee_date');
+Route::get('payroll/{management_id}/{from_date}/{to_date}','ReportExcelController@payroll');
+
 //** Revisar  */
 Route::get('ReportMonth/{fecha}/{id_user}/{planta}', 'ReportController@ReportMonth');
 Route::get('ReportYear/{fecha}/{id_user}/{planta}', 'ReportController@ReportYear');
